337f14c9a57da25ca929b89f5b1429ef0af845df
rafaadip <39415423+rafaadip@users.noreply.github.com>
2023-09-15 16:41:54 -0300

    Create Aula4.py

---
+++ Aula4.py
@@ -0,0 +1,37 @@
+#############################
+### Exemplos condicionais ###
+#############################
+# # Textos e números
+# print(n2, n1)
+# # Textos e números
+# print(f'{n2}{n1}')
+# #Com números reais
+# print("hello %s %d"%(n1,n2))
+# # Com números reais e textos
+# print("Hello {}{}".format(n1,n2))
+
+# Exercício 1: Verificação de Número Positivo
+# Escreva um programa em Python que solicite ao usuário um número inteiro e verifique se o número é positivo, negativo ou zero. 
+# Em seguida, exiba uma mensagem apropriada de acordo com o resultado da verificação.
+
+# num1 = int(input("Digite um número: "))
+# if num1 > 0:
+#     print("O número digitado é positivo")
+# elif num1 < 0:
+#         print("O número digitado é negativo")
+# else:
+#             print("O número é 0")
+
+# Exercício 2: Classificação de Triângulos
+
+# Escreva um programa em Python que receba três comprimentos de lados de um triângulo e determine se o triângulo é equilátero, isósceles ou escaleno.
+
+# a = float(input("Digite o primeiro comprimento do seu trinagulo: "))
+# b = float(input("Digite o segundo comprimento do seu trinagulo: "))
+# c = float(input("Digite o terceiro comprimento do seu trinagulo: "))
+# if a == b == c:
+#     print("O triangulo é equilatero!")
+# elif a == b or b == a or b == c:
+#         print("O triangulo é isósceles!")
+# else:
+#             print("O trinagulo é escaleno!")
